002f49c3d05c1b1492952d606f2e5c69dcf6be3f
Andrew Smith <ajs3993@gmail.com>
2019-10-15 17:20:22 -0500

    created game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -4,55 +4,62 @@
 
 var cards = [
     {
-    rank: 'queen',
-    suit: 'hearts',
-    cardImage: "images/queen-of-hearts.png"
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: "images/queen-of-hearts.png"
     },
     {
-    rank: 'queen',
-    suit: 'diamonds',
-    cardImage: "images/queen-of-diamonds.png"
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: "images/queen-of-diamonds.png"
     },
     {
-    rank: 'king',
-    suit: 'hearts',
-    cardImage: "images/king-of-hearts.png"
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: "images/king-of-hearts.png"
     },
     {
-    rank: 'king',
-    suit: 'diamonds',
-    cardImage: "images/king-of-diamonds.png"
-            }
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: "images/king-of-diamonds.png"
+    }
     
 ];
     
 
-const cardsInPlay = [];
+var cardsInPlay = [];
+
+function createBoard(){
+    for (var i = 0; i < cards.length; i++) {
+        
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById("game-board").appendChild(cardElement)
+
+    }
+}
 
 function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+    
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }  
 }
 
-function flipCard(cardId){
+function flipCard(){
 
-    var cardOne = cards[0];
+    cardId = this.getAttribute("data-id")
+    this.setAttribute('src', cards[cardId].cardImage)
+    var cardOne = cards[cardId];
     cardsInPlay.push(cardOne);
-    console.log("User flipped " + cardOne);
-    
-    var cardTwo = cards[2];
-    cardsInPlay.push(cardTwo);
-    console.log("User flipped " + cardTwo);
-
-    console.log("User flipped" + cards[cardId].rank)
-
-    cardsInPlay.push('cards[cardId].rank');
-
+    console.log("User flipped " + cards[cardId].rank)
+    if (cardsInPlay.length === 2){
     checkForMatch();
 }
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
