4b0fc169c023d107c16fe034821f0a4e873f9fcf
Andrew Smith <ajs3993@gmail.com>
2019-10-15 15:27:30 -0500

    created game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -2,7 +2,31 @@
 
 
 
-const cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: "images/king-of-hearts.png"
+    },
+    {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: "images/king-of-diamonds.png"
+            }
+    
+];
+    
+
 const cardsInPlay = [];
 
 function checkForMatch(){
@@ -23,9 +47,9 @@ function flipCard(cardId){
     cardsInPlay.push(cardTwo);
     console.log("User flipped " + cardTwo);
 
-    console.log("User flipped" + cards[cardId])
+    console.log("User flipped" + cards[cardId].rank)
 
-    cardsInPlay.push('cards[cardId]');
+    cardsInPlay.push('cards[cardId].rank');
 
     checkForMatch();
 }
